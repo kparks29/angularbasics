0f6caefb1e96d695f0633c4aac77cd93733f0e9e
Kendrick Parks <bordersmbk02@aol.com>
2014-01-14 16:02:13 -0800

    about to begin lorin's lesson

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function myController($scope) {
+	$scope.cats = [
+		{text: "Pickle", done:false},
+		{text: "Ben", done:false},
+		{text: "Ethel", done:false},
+		{text: "Lil Bub", done:false},
+		{text: "Grumpy Cat", done:false}
+	];
+}
