92455c0ed4592bedd0b92cdac0d76f0d254ed838
Kendrick Parks <bordersmbk02@aol.com>
2014-01-14 20:43:42 -0800

    toggle works

--- js/script.js
+++ js/script.js
@@ -1,39 +1,14 @@
+var tog = 'O';
+
 function myController($scope) {
-	$scope.cats = [
-		{text: "Pickle", done:false},
-		{text: "Ben", done:false},
-		{text: "Ethel", done:false},
-		{text: "Lil Bub", done:false},
-		{text: "Grumpy Cat", done:false}
-	];
+	$scope.ar = [0,1,2];
+	$scope.toggler = function(i,j) {
+		var cell = document.getElementById('cell' + i + '-' + j);
+		if (cell.innerHTML == "") {
+			tog = (tog == 'X' ? 'O' : 'X');
+			cell.innerHTML = tog;
+		}
+	};
+
 }
 
-
-
-
-/*
-<div>
-		<ul x-ng-controller="myController">
-			<h1 x-ng-bind="catName"></h1>
-			{{catName}}<br />
-			{{catName}}
-			<li x-ng-repeat="cat in cats">
-				<input type="checkbox" x-ng-model="cat.done" />
-				<span class="done-{{cat.done}}">{{cat.text}}</span>
-			</li>
-		</ul>
-		<input type="text" x-ng-model="catName" />
-		<input type="text" x-ng-model="catName2" />
-	</div>
-
-	<div>
-		<select x-ng-model="catName">
-			<option>One</option>
-			<option>Two</option>
-			<option>Three</option>
-			<option>true</option>
-			<option>false</option>
-		</select>
-		<input type="checkbox" x-ng-checked="catName == 'cat'">
-	</div>
-	*/
