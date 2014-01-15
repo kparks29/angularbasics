353f72064f0fa0a186533bcd8d5bad5cfa3a7882
Kendrick Parks <bordersmbk02@aol.com>
2014-01-14 17:12:02 -0800

    clickable button

--- js/script.js
+++ js/script.js
@@ -7,3 +7,33 @@ function myController($scope) {
 		{text: "Grumpy Cat", done:false}
 	];
 }
+
+
+
+
+/*
+<div>
+		<ul x-ng-controller="myController">
+			<h1 x-ng-bind="catName"></h1>
+			{{catName}}<br />
+			{{catName}}
+			<li x-ng-repeat="cat in cats">
+				<input type="checkbox" x-ng-model="cat.done" />
+				<span class="done-{{cat.done}}">{{cat.text}}</span>
+			</li>
+		</ul>
+		<input type="text" x-ng-model="catName" />
+		<input type="text" x-ng-model="catName2" />
+	</div>
+
+	<div>
+		<select x-ng-model="catName">
+			<option>One</option>
+			<option>Two</option>
+			<option>Three</option>
+			<option>true</option>
+			<option>false</option>
+		</select>
+		<input type="checkbox" x-ng-checked="catName == 'cat'">
+	</div>
+	*/
